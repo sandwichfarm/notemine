c9ad529c9a52eb4eafde6d1d53ae6edf8cac211c
dskvr <dskvr@users.noreply.github.com>
2024-11-05 00:11:52 +0100

    remove docs build

--- docs/build/assets/js/a7456010.5e5ab375.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_notemine_docs=self.webpackChunk_notemine_docs||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docs/build/assets/js/aba21aa0.46d9a977.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_notemine_docs=self.webpackChunk_notemine_docs||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);

5d6d063f0c9fe170ea26c91b7d89da3abc99eb39
Sandwich <299465+dskvr@users.noreply.github.com>
2025-01-19 07:58:50 +0700

    move rust to core

--- packages/core/scripts/build.sh
+++ packages/core/scripts/build.sh
@@ -10,14 +10,13 @@ function error {
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 RUST_DIR="$SCRIPT_DIR/.."
-CORE_DIR="$SCRIPT_DIR/../../core"
-SCRIPT_UPDATE_PACKAGE="$SCRIPT_DIR/update-package-json.mjs"
+# SCRIPT_UPDATE_PACKAGE="$SCRIPT_DIR/update-package-json.mjs"
 README_NPM="$RUST_DIR/README.npm.md"
 
 [ -d "$RUST_DIR" ] || error "Rust directory not found at $RUST_DIR"
-[ -d "$CORE_DIR" ] || echo "Core directory not found at $CORE_DIR, so creating it..."
-mkdir -p "$CORE_DIR"
-[ -f "$SCRIPT_UPDATE_PACKAGE" ] || error "Update package script not found at $SCRIPT_UPDATE_PACKAGE"
+# [ -d "$CORE_DIR" ] || echo "Core directory not found at $CORE_DIR, so creating it..."
+# mkdir -p "$CORE_DIR"
+# [ -f "$SCRIPT_UPDATE_PACKAGE" ] || error "Update package script not found at $SCRIPT_UPDATE_PACKAGE"
 [ -f "$README_NPM" ] || error "README.npm.md not found at $README_NPM"
 
 command -v cargo >/dev/null 2>&1 || error "cargo is not installed."
@@ -56,16 +55,16 @@ echo "Renamed pkg to dist"
 
 echo "Updating package.json..."
 
-if [ ! -x "$SCRIPT_UPDATE_PACKAGE" ]; then
-  chmod +x "$SCRIPT_UPDATE_PACKAGE"
-  echo "Set execute permission for update-package-json.mjs"
-fi
+# if [ ! -x "$SCRIPT_UPDATE_PACKAGE" ]; then
+#   chmod +x "$SCRIPT_UPDATE_PACKAGE"
+#   echo "Set execute permission for update-package-json.mjs"
+# fi
 
-"$SCRIPT_UPDATE_PACKAGE"
-echo "package.json has been successfully updated."
+# "$SCRIPT_UPDATE_PACKAGE"
+# echo "package.json has been successfully updated."
 
-echo "Copying dist to packages/core..."
-cp -fr "$RUST_DIR/dist/." "$CORE_DIR"
-echo "Copied dist to packages/core"
+# echo "Copying dist to packages/core..."
+# cp -fr "$RUST_DIR/dist/." "$CORE_DIR"
+# echo "Copied dist to packages/core"
 
 echo "Build process successfully completed."
